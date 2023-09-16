3cf2dab28e8913d45c29858b2ab69ad4d4a2b245
SeongUk18 <adulty22@gmail.com>
2023-09-16 18:48:36 +0900

    3week
    3week

--- routes/books.js
+++ routes/books.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET 도서검색 page. */
 router.get('/', function(req, res, next) {
-  // '/' 경로 의미
+  // '/' 경로 의미 -> 라우터 등록시 books로 저장했기때문에 books 생략됨
   res.render('index', { title: '도서검색', pageName:'books/search.ejs'});
   // index.ejs 파일 가져온다는 의미
 });
